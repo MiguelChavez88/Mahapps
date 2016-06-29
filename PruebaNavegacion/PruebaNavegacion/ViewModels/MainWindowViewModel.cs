22e45188d98b48a1c2b4aa2c3653663cca2414d9
machavez <chavz88@gmail.com>
2016-06-29 16:51:05 -0600

    Navegacion

---
+++ PruebaNavegacion/PruebaNavegacion/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using PruebaNavegacion.Views;
+
+
+namespace PruebaNavegacion.ViewModels
+{
+    class MainWindowViewModel:ViewModelBase
+    {
+        private readonly Action<UserControl> _navigateToView;
+        private Editar _editarView;
+        public RelayCommand NewMatch { get; set; }
+
+        public MainWindowViewModel(Action<UserControl> navigateToView)
+        {
+            this._navigateToView = navigateToView;
+            Initializate();
+            NewMatch = new RelayCommand(_ => navigateToView(_editarView));
+        }
+
+        private void Initializate()
+        {
+            _editarView = new Editar();
+        }
+    }
+}
